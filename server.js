5dc756d9af8b669af1eec8135a04b74ccc23047d
OrigamiStarz <isabelbku@gmail.com>
2023-10-28 16:51:11 -0700

    Center footer better and broke scraper

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const fileUpload = require('express-fileupload');
 const app = express();
 const bodyParser =  require("body-parser");
+const axios = require('axios');
+const cheerio = require('cheerio');
 const crawlee = require("crawlee")
-
+const Apify = require("apify-cli")
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
@@ -38,7 +40,7 @@ app.get("/website", function(req, res) {
 
 app.post("/website", async function(req, res) {
     // use scraper
-    scraped = await scrape(req.body["url"],req.body["numCrawl"]);
+    scraped = await scrape(req.body["url"], 0, req.body["numCrawl"]);
     res.send(scraped);
 });
 
@@ -53,22 +55,48 @@ app.listen(process.env.PORT || 3000, function() {
 
 // https://stackoverflow.com/questions/23691194/node-express-file-upload
 
-async function scrape(url, numCrawl) {
+async function scrape(url, depth, numCrawl) {
     if (!numCrawl) numCrawl = 1;
     numCrawl = parseInt(numCrawl);
     numCrawl = (numCrawl >= 1 && numCrawl < 10) ? numCrawl : 1;
-    allContent = []
-    const crawler = new crawlee.CheerioCrawler({
-        // limit requests
-        maxRequestsPerCrawl: numCrawl,
-        // start crawling
-        async requestHandler({ $, request, enqueueLinks }) {
-            const content = $('body').text();
-            allContent.push(content);
-            // keep crawling links
-            await enqueueLinks();
+    content = await scrapeWithDepth(url, depth, numCrawl)
+    return {content:content}
+}
+
+async function scrapeWithDepth(url, depth, numCrawl) {
+    // scrape a URL up to a maximum depth
+    if (depth >= numCrawl) {
+        return; // exit if maximum depth
+    }
+    // fetch html
+    content = []
+    await axios.get(url)
+    .then((response) => {
+    if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        // Use Cheerio to select and extract data from the HTML
+        content.push($.text());
+        // Find links on the page and recursively scrape them
+        const links = [];
+        $('a').each((index, element) => {
+        const link = $(element).attr('href');
+        if (link && link.startsWith('http')) {
+            links.push(link);
         }
+        });
+        if (depth + 1 < numCrawl) {
+            // recursive scrape
+            links.forEach((link) => {
+                console.log(link)
+                let next = scrapeWithDepth(link, depth + 1, numCrawl);
+                if (next) content.push(next);
+            });
+        }
+    }
+    })
+    .catch((error) => {
+        return {content: error};
     });
-    await crawler.run([url]);
-    return {content:allContent};
+    return content;
 }
