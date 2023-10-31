f5f4548fffc5533620f70358acfc3f05247be669
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2023-10-30 19:11:39 -0700

    Fix file collisions for documents feature

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 const app = express();
 const bodyParser =  require("body-parser");
-const axios = require('axios').default;
-const cheerio = require('cheerio');
-const fs = require('fs').promises;
+const axios = require("axios").default;
+const cheerio = require("cheerio");
+const fs = require("fs").promises;
 const mammoth = require("mammoth")
-const pdf = require('pdf-parse');
+const pdf = require("pdf-parse");
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // for environmental variables
-require('dotenv').config()
+require("dotenv").config()
 AI_API_TOKEN = process.env.AI_API_TOKEN
 AI_API_TEST_TOKEN = process.env.AI_API_TEST_TOKEN
 
@@ -34,54 +34,55 @@ app.get("/document", function(req, res) {
     res.sendFile(__dirname + "/public/document.html");
 });
 
-app.post('/document', async (req, res) => {
+app.post("/document", async (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).json({ error: 'No files were uploaded.' });
+      return res.status(400).json({ error: "No files were uploaded." });
     }
   
     const file = req.files.file;
   
     try {
-      await file.mv('uploads/' + file.name);
-      const filePath = __dirname + '/uploads/' + file.name;
+      const timeStamp = Date.now();
+      await file.mv("uploads/" + timeStamp + "_" + file.name);
+      const filePath = __dirname + "/uploads/" +  timeStamp + "_" + file.name;
   
-      if (file.name.endsWith('.pdf')) {
+      if (file.name.endsWith(".pdf")) {
         try {
           const dataBuffer = await fs.readFile(filePath);
           const data = await pdf(dataBuffer);
           const textContent = data.text;
           const processedData = await processData(textContent);
-          res.json({ message: 'File uploaded and processed successfully', content: processedData });
+          res.json({ message: "File uploaded and processed successfully", content: processedData });
         } catch (error) {
-          console.error('Error processing PDF:', error);
-          res.status(500).json({ error: 'Error processing PDF: ' + error.message });
+          console.error("Error processing PDF:", error);
+          res.status(500).json({ error: "Error processing PDF: " + error.message });
         }
-      } else if (file.name.endsWith('.docx') || file.name.endsWith('.doc')) {
+      } else if (file.name.endsWith(".docx") || file.name.endsWith(".doc")) {
         try {
           const dataBuffer = await fs.readFile(filePath);
           const result = await mammoth.extractRawText({ buffer: dataBuffer });
           const textContent = result.value;
           const processedData = await processData(textContent);
-          res.json({ message: 'File uploaded and processed successfully', content: processedData });
+          res.json({ message: "File uploaded and processed successfully", content: processedData });
         } catch (error) {
-          console.error('Error processing Word document:', error);
-          res.status(500).json({ error: 'Error processing Word document: ' + error.message });
+          console.error("Error processing Word document:", error);
+          res.status(500).json({ error: "Error processing Word document: " + error.message });
         }
-      } else if (file.name.endsWith('.txt')) {
+      } else if (file.name.endsWith(".txt")) {
         try {
-          const textContent = await fs.readFile(filePath, 'utf8');
+          const textContent = await fs.readFile(filePath, "utf8");
           const processedData = await processData(textContent);
-          res.json({ message: 'File uploaded and processed successfully', content: processedData });
+          res.json({ message: "File uploaded and processed successfully", content: processedData });
         } catch (error) {
-          console.error('Error processing text file:', error);
-          res.status(500).json({ error: 'Error processing text file: ' + error.message });
+          console.error("Error processing text file:", error);
+          res.status(500).json({ error: "Error processing text file: " + error.message });
         }
       } else {
-        res.status(400).json({ error: 'Unsupported file format.' });
+        res.status(400).json({ error: "Unsupported file format." });
       }
     } catch (err) {
-      console.error('File upload error:', err);
-      res.status(500).json({ error: 'File upload error: ' + err.message });
+      console.error("File upload error:", err);
+      res.status(500).json({ error: "File upload error: " + err.message });
     }
   });  
 
@@ -93,7 +94,7 @@ app.get("/website", function(req, res) {
 app.post("/website", async function(req, res) {
     // use scraper
     let numCrawl = req.body["numCrawl"];
-    if (numCrawl === undefined || numCrawl === null || numCrawl === '' || isNaN(numCrawl)) {
+    if (numCrawl === undefined || numCrawl === null || numCrawl === "" || isNaN(numCrawl)) {
       numCrawl = 1;
     } else {
       numCrawl = parseInt(numCrawl);    
@@ -124,7 +125,7 @@ function processData(text) {
         method: "POST",
         url: "https://api.edenai.run/v2/text/anonymization",
         headers: {
-          authorization: "Bearer " + AI_API_TOKEN,
+          authorization: "Bearer " + AI_API_TEST_TOKEN,
         },
         data: {
           show_original_response: false,
@@ -178,7 +179,7 @@ function processData(text) {
 
 async function scrape(url, depth) {
   if (depth <= 0) {
-    return '';
+    return "";
   }
 
   try {
@@ -188,19 +189,19 @@ async function scrape(url, depth) {
       const $ = cheerio.load(html);
 
       // Select and extract text content from specific tags (e.g., p, h1, h2)
-      const textContent = $('p, h1, h2').text();
+      const textContent = $("p, h1, h2").text();
       console.log(`Scraped: ${url}`);
 
       // Find links on the page and follow them recursively
       const links = [];
-      $('a').each((index, element) => {
-        const link = $(element).attr('href');
-        if (link && link.startsWith('http')) {
+      $("a").each((index, element) => {
+        const link = $(element).attr("href");
+        if (link && link.startsWith("http")) {
           links.push(link);
         }
       });
 
-      let subContent = '';
+      let subContent = "";
       for (const link of links) {
         try {
           subContent += await scrape(link, depth - 1);
@@ -213,10 +214,10 @@ async function scrape(url, depth) {
       return textContent + subContent;
     } else {
       console.error(`Failed to fetch the website: ${url}`);
-      return '';
+      return "";
     }
   } catch (error) {
     console.error(`Error while scraping ${url}: ${error.message}`);
-    return '';
+    return "";
   }
 }
